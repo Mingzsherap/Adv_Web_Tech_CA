1017915d1e7497c953999e3c92cc00fcd6a794e3
msher <msher@LAPTOP-DE465T1J>
2023-04-08 23:33:14 +0100

    Add project files.

---
+++ Adv_Web_Tech_CA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Adv_Web_Tech_CA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
